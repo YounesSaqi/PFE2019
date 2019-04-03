6a22638dd381dcbbb780ecc21230450b67a12a98
YOUNES SAQI <33899740+YounesSaqi@users.noreply.github.com>
2019-04-03 14:18:25 +0100

    Add files via upload

--- demo/src/main/java/com/example/demo/Controller/SShController.java
+++ demo/src/main/java/com/example/demo/Controller/SShController.java
@@ -2,11 +2,15 @@ package com.example.demo.Controller;
 
 
 import com.example.demo.DAO.SShDAO;
+import com.example.demo.Entities.Commande;
 import com.example.demo.Entities.SSHConnection;
 import com.jcraft.jsch.*;
+import com.jcraft.jsch.ChannelSftp;
+import com.jcraft.jsch.Channel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
 import java.io.InputStream;
 
 @RestController
@@ -18,6 +22,10 @@ public class SShController {
     @Autowired
     private SShDAO sshDAO;
     private Session session;
+
+    public SShController() {
+    }
+
     @RequestMapping(value = "", method = RequestMethod.POST)
     public SSHConnection saveSSH(@RequestBody SSHConnection c) {
         return sshDAO.saveSshConnection(c);
@@ -25,59 +33,80 @@ public class SShController {
 
     //connexion vers VM
     @RequestMapping(value = "/connect", method = RequestMethod.POST)
-    public Session EtablirConn(@RequestBody SSHConnection c) {
-        JSch jsch = new JSch();
-
+    public Session connect(@RequestBody SSHConnection c) throws JSchException {
         try {
             java.util.Properties config = new java.util.Properties();
             config.put("StrictHostKeyChecking", "no");
-            this.session = jsch.getSession(c.getUser(), c.getHost(), 22);
+            JSch jsch = new JSch();
+
+             session = jsch.getSession(c.getUser(), c.getHost(), 22);
             session.setPassword(c.getPassword());
             session.setConfig(config);
             session.connect();
-            System.out.println("Connected");
+
+           System.out.println("connect");
             return session;
-        } catch (JSchException e) {
-          //  e.printStackTrace();
-            System.out.println(" Non Connected");
+        }catch(Exception e){
+            System.out.println("Non Connected");
+
             return null;
         }
-
     }
 
-//Excution des commandes
-    @RequestMapping(value = "/commande", method = RequestMethod.POST)
-    public void ExcutionCommande(@RequestParam String commande) {
-     try{
-         Channel channel=session.openChannel("exec");
-         ((ChannelExec)channel).setCommand(commande);
-         channel.setInputStream(null);
-         ((ChannelExec)channel).setErrStream(System.err);
 
-         InputStream in=channel.getInputStream();
-         channel.connect();
-         byte[] tmp=new byte[4048];
-         while(true){
-             while(in.available()>0){
-                 int i=in.read(tmp, 0, 4048);
-                 if(i<0)break;
-                 System.out.print(new String(tmp, 0, i));
-             }
-             if(channel.isClosed()){
-                 System.out.println("exit-status: "+channel.getExitStatus());
-                 break;
-             }
-             try{Thread.sleep(1000);}catch(Exception ee){}
-         }
-            }catch (Exception e) {
-            e.printStackTrace();
-        }
+    //Excution des commandes
+   @RequestMapping(value = "/commande", method = RequestMethod.POST)
+       public  Commande ExecuteCommande(@RequestBody Commande commande) {
+           String CommandOutput = null;
+           try {
+               // System.out.println("Connected");
+               Channel channel = session.openChannel("exec");
+               ((ChannelExec)channel).setPty(true);
+               InputStream in = channel.getInputStream();
+               channel.setInputStream(null);
+                ((ChannelExec) channel).setErrStream(System.err);
+
+               ((ChannelExec) channel).setCommand(commande.getCommande());
+               ((ChannelExec) channel).setErrStream(System.err);
 
 
+               channel.connect();
+               byte[] tmp = new byte[1024];
+               while (true) {
+                   while (in.available() > 0) {
+                       int i = in.read(tmp, 0, 1024);
 
-        }
+                       if (i < 0)
+                           break;
+                       // System.out.print(new String(tmp, 0, i));
+                       CommandOutput = new String(tmp, 0, i);
+                   }
 
- //copy fichier local vers server linux
+                   if (channel.isClosed()) {
+                       // System.out.println("exit-status: " +
+                       // channel.getExitStatus());
+                       break;
+                   }
+                   try {
+                       Thread.sleep(1000);
+                   } catch (Exception ee) {
+                   }
+               }
+            //   channel.disconnect();
+           //    session.disconnect();
+               // System.out.println("DONE");
+
+           } catch (Exception e) {
+               e.printStackTrace();
+           }
+       commande.setCommande(CommandOutput);
+       System.out.println(commande.getCommande());
+           return commande;
+
+       }
+
+
+    //copy fichier local vers server linux
     @RequestMapping(value = "/copy", method = RequestMethod.POST)
     public void copyfichier(String local,String host){
 
